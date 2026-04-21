77b8cb99c0fdf4dfddd93e357c3cfc86b1b7096a
Mukesh Kumar <mukeshkumarsingh7488@gmail.com>
2026-04-21 20:35:19 +0530

    Updated CORS for production

--- server.js
+++ server.js
@@ -22,12 +22,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://127.0.0.1:5500",
-      "http://localhost:5500",
-      "http://127.0.0.1:56680",
-      "http://127.0.0.1:62115",
-    ],
+    origin: ["http://127.0.0.1:5500", "https://br-30-kart.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -39,12 +34,7 @@ app.set("socketio", io);
 /* ================== MIDDLEWARES ================== */
 app.use(
   cors({
-    origin: [
-      "http://127.0.0.1:5500",
-      "http://localhost:5500",
-      "http://127.0.0.1:56680",
-      "http://127.0.0.1:62115",
-    ],
+    origin: ["http://127.0.0.1:5500", "https://br-30-kart.vercel.app"],
     credentials: true,
   }),
 );
